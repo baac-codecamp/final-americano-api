5557a814384a6dffa86d8855611743f1a8a8fbe6
Kraisit.ch <kraisit.ch@baac.or.th>
2020-08-06 14:36:19 +0700

    [Edited]: Update function InsertDataReward to req realworld

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -5,6 +5,83 @@ const CLT_News = require('../models/news')
 const path = require('path')
 const _baseCore = require('../utils/baseCore')
 
+// async function insertDataCustomer() {
+//   const pathData = path.join(__dirname, '../data/customer.xlsx')
+//   const wb = XLSX.readFile(pathData)
+//   const wsData = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]])
+
+//   for (const itemWs1 of wsData) {
+//     let currCus = await CLT_CUSTOMER.findOne({ cid: itemWs1.CID })
+
+//     if (currCus) {
+//       // Update Customer
+
+//       let isFoundAccNo = await currCus.cusSalak.find((itemCSLK1) => itemCSLK1.accNo === itemWs1.AccNo)
+
+//       if (isFoundAccNo) {
+//         // console.log('isFoundAccNo')
+
+//         let isFoundSalakNo = await isFoundAccNo.salakNo.find((itemSLK1) => itemSLK1.start === itemWs1.SalakStart && itemSLK1.end === itemWs1.SalakEnd)
+
+//         if (!isFoundSalakNo) {
+//           // console.log('!isFoundSalakNo')
+
+//           // Add SalakNo
+//           isFoundAccNo.salakNo.push({ start: itemWs1.SalakStart, end: itemWs1.SalakEnd })
+//           await currCus.save()
+//           console.log('Add New SalakNo')
+//           console.log('UPDATE: ' + itemWs1.CID)
+//           console.log('SAVE!')
+//         }
+//       } else {
+//         // Add New Account
+//         // console.log('Add New Account')
+
+//         currCus.cusSalak.push({
+//           accNo: itemWs1.AccNo,
+//           accName: itemWs1.AccName.trim(),
+//           accType: itemWs1.AccType,
+//           salakNo: [
+//             {
+//               start: itemWs1.SalakStart,
+//               end: itemWs1.SalakEnd,
+//             },
+//           ],
+//         })
+
+//         await currCus.save()
+//         console.log('Add New Account')
+//         console.log('UPDATE: ' + itemWs1.CID)
+//         console.log('SAVE!')
+//       }
+//     } else {
+//       // Add New Customer
+//       let objCus1 = new CLT_CUSTOMER()
+//       objCus1.cid = itemWs1.CID
+//       objCus1.cif = itemWs1.CIFNo
+//       objCus1.bod = itemWs1.BOD
+//       objCus1.cusName = itemWs1.CIFName
+//       objCus1.cusSalak = [
+//         {
+//           accNo: itemWs1.AccNo,
+//           accName: itemWs1.AccName.trim(),
+//           accType: itemWs1.AccType,
+//           salakNo: [
+//             {
+//               start: itemWs1.SalakStart,
+//               end: itemWs1.SalakEnd,
+//             },
+//           ],
+//         },
+//       ]
+//       await objCus1.save()
+//       console.log('Add New Customer')
+//       console.log('ADD: ' + itemWs1.CID)
+//       console.log('SAVE!')
+//     }
+//   }
+// }
+
 async function insertDataCustomer() {
   const pathData = path.join(__dirname, '../data/customer.xlsx')
   const wb = XLSX.readFile(pathData)
@@ -82,18 +159,42 @@ async function insertDataCustomer() {
   }
 }
 
-async function insertDataReward() {
-  const pathData = path.join(__dirname, '../data/reward.xlsx')
-  const wb = XLSX.readFile(pathData)
-  const wsData = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]])
+// async function insertDataReward() {
+//   const pathData = path.join(__dirname, '../data/reward.xlsx')
+//   const wb = XLSX.readFile(pathData)
+//   const wsData = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]])
 
+//   let currData = await CLT_REWARD.find()
+//   for (const item1 of currData) {
+//     await CLT_REWARD.findByIdAndDelete({ _id: item1._id })
+//   }
+//   console.log('Delete Data Reward Success!')
+
+//   for (const itemWs1 of wsData) {
+//     let objRew1 = new CLT_REWARD()
+//     const { RewardAtDate, RewardAtSeq, RewardPrice, RewardNo } = itemWs1
+//     objRew1.rewardAtDate = new Date(RewardAtDate)
+//     objRew1.rewardAtSeq = RewardAtSeq
+//     objRew1.rewardPrice = RewardPrice
+//     objRew1.rewardNo = RewardNo
+//     objRew1.save()
+//     // console.log('Add New Data Reward')
+//     // console.log('SAVE!')
+//   }
+
+//   console.log('Add New Reward Success!')
+// }
+
+async function insertDataReward(req, res, next) {
+  console.log('Hi inser Reward')
+  const { listDataReward } = req.body
   let currData = await CLT_REWARD.find()
   for (const item1 of currData) {
     await CLT_REWARD.findByIdAndDelete({ _id: item1._id })
   }
   console.log('Delete Data Reward Success!')
 
-  for (const itemWs1 of wsData) {
+  for (const itemWs1 of listDataReward) {
     let objRew1 = new CLT_REWARD()
     const { RewardAtDate, RewardAtSeq, RewardPrice, RewardNo } = itemWs1
     objRew1.rewardAtDate = new Date(RewardAtDate)
@@ -104,8 +205,8 @@ async function insertDataReward() {
     // console.log('Add New Data Reward')
     // console.log('SAVE!')
   }
-
   console.log('Add New Reward Success!')
+  return _baseCore.resMsg(res, 200, 'S', 'Add New Reward Success!', {})
 }
 
 async function addNews(req, res, next) {
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -11,6 +11,7 @@ router.post('/signup', SYSTEM_BL.signup)
 router.post('/login', SYSTEM_BL.login)
 
 router.post('/addNews', ADMIN_BL.addNews)
+router.post('/insertDataReward', ADMIN_BL.insertDataReward)
 
 //router.get('/getNews/:id',ADMIN_BL.getNewsById)
 module.exports = router
